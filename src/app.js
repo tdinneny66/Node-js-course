5e180b94c00bfc6e77335820d298237bc47b4ef8
tdinneny66 <tdinneny@optonline.net>
2021-06-16 12:09:21 -0400

    Fixed PORT

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 //Define Paths for app config
 const publicDirPath = path.join(__dirname, '../public')
